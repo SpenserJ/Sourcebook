78672f28c4099f437f875c4d974f3cadd626cc38
Spenser Jones <spenser.e.jones@gmail.com>
2013-02-05 20:16:29 -0700

    Sourcebook tracks, commits, and updates now

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-  function CommitModel(changeType, filename) {
+  function CommitModel(timestamp, changeType, filename) {
+    this.timestamp = ko.observable(timestamp);
     this.changeType = ko.observable(changeType);
     this.filename = ko.observable(filename);
   };
 
   function CommitLogModel() {
     var self = this;
-    self.commits = ko.observableArray([
-      new CommitModel("Add", "C:\\boom.txt")
-    ]);
+    self.commits = ko.observableArray([]);
   }
 
   var commitLog = new CommitLogModel();
@@ -17,7 +16,7 @@ $(document).ready(function() {
 
   var socket = io.connect();
   socket.on('change', function(data) {
-    commitLog.commits.push(new CommitModel(data.type, data.path));
+    commitLog.commits.push(new CommitModel(new Date(), data.type, data.path));
   }).on('initialize', function(data) {
     console.log(data);
   });
